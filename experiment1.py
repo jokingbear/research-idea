6fefbd9490ca4dad7816bc361f11d22e6538085f
jokingbear <nerokid1072@gmail.com>
2019-08-06 18:52:41 +0700

    combine routing and group conv

---
+++ experiment1.py
@@ -0,0 +1,10 @@
+from keras import layers, Model
+from implementations import conv_routing as cr, group_conv as gc
+
+x = layers.Input([512, 512, 128])
+
+y = cr.GroupRouting(32, 18, 3)(x)
+
+model = Model(x, y)
+
+w = model.get_weights()
--- implementations/conv_routing.py
+++ implementations/conv_routing.py
@@ -17,15 +17,12 @@ class ConvRouting(layers.Layer):
         shape = K.int_shape(inputs)
         g = self.n_group
         c = shape[-1] // g
-        inputs = K.reshape(inputs, shape[:-1] + (g, c))
+        inputs = K.reshape(inputs, (-1,) + shape[1:-1] + (g, c))
 
         beta = K.zeros([1, 1, 1, g, 1])
-        i0 = K.constant(0)
-        v, _ = tf.while_loop(lambda i, *args: i < self.n_iter, self._routing, [i0, inputs, beta],
-                             shape_invariants=[tf.TensorShape(0), tf.TensorShape([None] + shape[1:-1] + [1, c]),
-                                               tf.TensorShape([None, 1, 1, g, 1])], swap_memory=True)
-        
-        return v[..., 0, :]
+        for i in range(self.n_iter):
+            alpha = K.sigmoid(beta)
+            pass
 
     def _routing(self, i, inputs, beta):
         alpha = K.sigmoid(beta)
@@ -33,3 +30,62 @@ class ConvRouting(layers.Layer):
 
         return tf.cond(i < self.n_iter - 1, lambda: (v, beta + K.sum(v * inputs, axis=(1, 2, -1))),
                        lambda: (v, beta))
+
+
+class GroupRouting(layers.Layer):
+
+    def __init__(self, n_group, n_filter, kernel, stride=1, padding="same", n_iter=3,
+                 kernel_initializer="he_normal", **kwargs):
+        self.n_group = n_group
+        self.n_filter = n_filter
+        self.kernel_sizes = kernel, kernel
+        self.strides = stride, stride
+        self.padding = padding
+        self.n_iter = n_iter
+        self.kernel_initializer = kernel_initializer
+
+        super().__init__(**kwargs)
+
+    def build(self, input_shape):
+        _, h, w, gp = input_shape
+        g = self.n_group
+        p = gp // g
+        f = self.n_filter
+
+        kh, kw = self.kernel_sizes
+
+        w = self.add_weight("weights", [kh, kw, gp, f], initializer="he_normal")
+        w = tf.reshape(w, [kw, kw, g, -1, f])
+        w = tf.transpose(w, [2, 0, 1, 3, 4])
+
+        ws = []
+        for i in range(g):
+            w_i = tf.pad(w[i], [(0, 0), (0, 0), (i * p, (g - i - 1) * p), (0, 0)])
+
+            ws.append(w_i)
+
+        self.w = tf.concat(ws, axis=-1)
+        self.b = self.add_weight("bias", [f], initializer="zeros")
+
+        super().build(input_shape)
+
+    def call(self, inputs, **kwargs):
+        conv = K.conv2d(inputs, self.w, self.strides, padding=self.padding)
+
+        _, h, w, gf = conv.shape.as_list()
+        conv = tf.reshape(conv, [-1, h, w, self.n_group, self.n_filter])
+        beta = K.zeros([self.n_group, 1])
+
+        for i in range(self.n_iter):
+            alpha = K.sigmoid(beta)
+            v = K.sum(alpha * conv, axis=-2, keepdims=True)
+
+            if i == self.n_iter - 1:
+                return v + self.b
+
+            beta = beta + K.sum(v * conv, axis=[1, 2, -1], keepdims=True)
+
+    def compute_output_shape(self, input_shape):
+        _, h, w, gp = input_shape
+
+        return None, h, w, self.n_filter
