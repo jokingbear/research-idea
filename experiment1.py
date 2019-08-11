5ac57ba60c3b13d3559bf7118b06845c5e9a6154
Simon <nerokid1072@gmail.com>
2019-08-11 21:21:42 +0700

    add data function to util

--- experiment1.py
+++
@@ -1,6 +0,0 @@
-from implementations import models
-from keras.utils import plot_model
-
-model = models.routing_encoder(f=32)
-
-plot_model(model, show_shapes=True)
--- implementations/utils.py
+++ implementations/utils.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def standardize_kernel_stride_dilation(dimensions, check_type, value):
     if type(value) is int:
         return (value,) * dimensions
@@ -6,3 +9,18 @@ def standardize_kernel_stride_dilation(dimensions, check_type, value):
     else:
         raise TypeError(f"{value} is not a valid {check_type}")
 
+
+def up_sample(data, n_sample):
+    n = len(data)
+
+    n_repeat = n_sample // n
+    n_add = n - n_sample % n
+
+    repeats = [np.random.choice(data, size=n, replace=False) for _ in range(n_repeat)]
+    adds = [np.random.choice(data, size=n_add, replace=False)]
+
+    return np.concatenate(repeats + adds)
+
+
+def normalize_img(img):
+    return img / 127.5 - 1
