cbf42ef8d3820437e257c6c5b3b08d1d23872d4e
Simon <nerokid1072@gmail.com>
2019-09-21 19:34:02 +0700

    refactor

--- torch_modules/routing_module.py
+++ torch_modules/routing_module.py
@@ -10,29 +10,29 @@ class DynamicRouting(nn.Module):
     def __init__(self, fi, fo, n_group, n_iter=3, bias=True):
         super().__init__()
 
-        shape = [fo * n_group, fi] + [1] * rank
+        spatial_shape = [1] * rank
+        shape = [fo * n_group, fi, *spatial_shape]
         self.n_group = n_group
         self.n_iter = n_iter
         self.weight = nn.Parameter(torch.Tensor(*shape))
-        self.bias = nn.Parameter(torch.Tensor(fo)) if bias else None
+        self.bias = nn.Parameter(torch.Tensor(1, fo, *spatial_shape)) if bias else None
 
         self.reset_parameters()
 
     def forward(self, x):
+        gfo, fi = self.weight.shape[:2]
+        g = self.n_group
+        fo = gfo // g
+
         if self.n_iter == 1:
-            gfo, fi = self.weight.shape[:2]
-            g = self.n_group
-            fo = gfo // g
             w = self.weight.reshape([g, fo, fi] + [1] * rank).transpose(0, 1).reshape([fo, -1] + [1] * rank)
             con = con_op(x, w, self.bias)
 
             return con
         else:
-            g = self.n_group
-            gf = self.weight.shape[0]
             con = con_op(x, self.weight, groups=self.n_group)
             spatial_shape = con.shape[2:]
-            con = con.reshape((-1, g, gf // g, *spatial_shape))
+            con = con.reshape((-1, g, fo, *spatial_shape))
 
             beta = torch.zeros([], device=x.device)
 
@@ -42,7 +42,7 @@ class DynamicRouting(nn.Module):
 
                 if i == self.n_iter - 1:
                     v = v[:, 0, ...]
-                    return v + self.bias.reshape([1, -1] + [1]*rank) if self.bias is not None else v
+                    return v if self.bias is None else v + self.bias
 
                 beta = beta + torch.sum(v * con, dim=(2,), keepdim=True)
 
