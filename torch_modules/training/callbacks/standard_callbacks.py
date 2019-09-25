a35db83ec2fa17e99032df25f4934e220d8fb4fc
Simon <nerokid1072@gmail.com>
2019-09-25 21:32:32 +0700

    add csv logger

--- torch_modules/training/callbacks/standard_callbacks.py
+++ torch_modules/training/callbacks/standard_callbacks.py
@@ -1,6 +1,12 @@
+import collections
+import io
+import os
+
 import numpy as np
+import six
 import torch
 import torch.optim.lr_scheduler as schedulers
+import csv
 
 from torch_modules.training.callbacks.root_class import Callback
 from collections import OrderedDict
@@ -20,7 +26,7 @@ class ReduceLROnPlateau(Callback):
 
     def on_train_begin(self):
         self.scheduler = schedulers.ReduceLROnPlateau(self.optimizer, mode=self.mode, factor=self.factor,
-                                                      patience=self.patience, verbose=self.verbose)
+                                                      patience=self.patience + 1, verbose=self.verbose > 1)
 
     def on_epoch_end(self, epoch, logs=None):
         self.scheduler.step(logs[self.monitor])
@@ -131,3 +137,93 @@ class Lookahead(Callback):
 
     def on_train_end(self):
         self.parameters = None
+
+
+class CSVLogger(Callback):
+    """Callback that streams epoch results to a csv file.
+
+  Supports all values that can be represented as a string,
+  including 1D iterables such as np.ndarray.
+
+  Example:
+
+  ```python
+  csv_logger = CSVLogger('training.log')
+  model.fit(X_train, Y_train, callbacks=[csv_logger])
+  ```
+
+  Arguments:
+      filename: filename of the csv file, e.g. 'run/log.csv'.
+      separator: string used to separate elements in the csv file.
+      append: True: append if file exists (useful for continuing
+          training). False: overwrite existing file,
+  """
+
+    def __init__(self, filename, separator=',', append=False):
+        self.sep = separator
+        self.filename = filename
+        self.append = append
+        self.writer = None
+        self.keys = None
+        self.append_header = True
+        if six.PY2:
+            self.file_flags = 'b'
+            self._open_args = {}
+        else:
+            self.file_flags = ''
+            self._open_args = {'newline': '\n'}
+        super().__init__()
+
+    def on_train_begin(self, logs=None):
+        if self.append:
+            if os.path.exists(self.filename):
+                with open(self.filename, 'r' + self.file_flags) as f:
+                    self.append_header = not bool(len(f.readline()))
+            mode = 'a'
+        else:
+            mode = 'w'
+        self.csv_file = io.open(self.filename,
+                                mode + self.file_flags,
+                                **self._open_args)
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+
+        def handle_value(k):
+            is_zero_dim_ndarray = isinstance(k, np.ndarray) and k.ndim == 0
+            if isinstance(k, six.string_types):
+                return k
+            elif isinstance(k, collections.Iterable) and not is_zero_dim_ndarray:
+                return '"[%s]"' % (', '.join(map(str, k)))
+            else:
+                return k
+
+        if self.keys is None:
+            self.keys = sorted(logs.keys())
+
+        if self.model.stop_training:
+            # We set NA so that csv parsers do not fail for this last epoch.
+            logs = dict([(k, logs[k]) if k in logs else (k, 'NA') for k in self.keys])
+
+        if not self.writer:
+
+            class CustomDialect(csv.excel):
+                delimiter = self.sep
+
+            fieldnames = ['epoch'] + self.keys
+
+            self.writer = csv.DictWriter(
+                self.csv_file,
+                fieldnames=fieldnames,
+                dialect=CustomDialect)
+            if self.append_header:
+                self.writer.writeheader()
+
+        row_dict = collections.OrderedDict({'epoch': epoch})
+        row_dict.update((key, handle_value(logs[key])) for key in self.keys)
+        self.writer.writerow(row_dict)
+        self.csv_file.flush()
+
+    def on_train_end(self, logs=None):
+        self.csv_file.close()
+        self.writer = None
