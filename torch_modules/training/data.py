08963f07733621c7d14539331ee1897871265fe1
jokingbear <nerokid1072@gmail.com>
2019-10-06 08:04:07 +0700

    add gan sequence

--- torch_modules/training/data.py
+++ torch_modules/training/data.py
@@ -44,3 +44,39 @@ class Sequence:
     @abstractmethod
     def shuffle(self):
         pass
+
+
+class GanSequence:
+
+    def __init__(self, x_type=torch.float, x_device=None):
+        self.x_type = x_type
+        self.x_device = x_device
+
+    def __len__(self):
+        return self.get_len()
+
+    def __getitem__(self, idx):
+        xs = self.get_item(idx)
+
+        if type(xs) is list:
+            xs = [torch.tensor(x, dtype=self.x_type, device=self.x_device) for x in xs]
+        else:
+            xs = torch.tensor(xs, dtype=self.x_type, device=self.x_device)
+
+        return xs
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+    @abstractmethod
+    def get_len(self):
+        pass
+
+    @abstractmethod
+    def get_item(self, idx):
+        pass
+
+    @abstractmethod
+    def shuffle(self):
+        pass
