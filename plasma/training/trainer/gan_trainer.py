5cdda9458894907b35346255105197c6059d831f
jokingbear <nerokid1072@gmail.com>
2019-11-17 19:16:02 +0700

    refactor gan trainer

--- plasma/training/trainer/gan_trainer.py
+++ plasma/training/trainer/gan_trainer.py
@@ -1,6 +1,7 @@
+from itertools import count
+
 import numpy as np
 import torch
-import torch.autograd as ag
 import torch.utils.data as data
 
 from plasma.training.trainer import utils
@@ -8,68 +9,66 @@ from plasma.training.trainer import utils
 
 class GANTrainer:
 
-    def __init__(self, discriminator, generator, d_optimizer, g_optimizer, loss, metrics=None,
-                 r1=None, real_label=1, fake_label=0, x_device=None):
+    def __init__(self, discriminator, generator_discriminator,
+                 discriminator_optimizer, generator_optimizer,
+                 loss, real_label=1, fake_label=0, regularizers=None,
+                 metrics=None, device=None):
         self.discriminator = discriminator
-        self.generator = generator
+        self.generator_discriminator = generator_discriminator
 
-        self.d_optimizer = d_optimizer
-        self.g_optimizer = g_optimizer
+        self.discriminator_optimizer = discriminator_optimizer
+        self.generator_optimizer = generator_optimizer
 
         self.loss = loss
-        self.metrics = metrics or []
-        self.r1 = r1 or 0
-
         self.real_label = real_label
         self.fake_label = fake_label
-        self.x_device = x_device
+        self.regularizers = regularizers or []
+
+        self.metrics = metrics or []
+        self.device = device
 
         self.d_kwargs = {}
         self.g_kwargs = {}
         self.stop_training = False
 
-    def fit(self, train, epochs=100, batch_size=32, g_batch_size=None, workers=0, callbacks=None):
-        sampler = train.get_sampler() if hasattr(train, "get_sampler") else None
-        loader = data.DataLoader(train, batch_size, sampler=sampler, shuffle=sampler is None, num_workers=workers,
-                                 pin_memory=True, drop_last=True)
-        self.g_kwargs["batch_size"] = g_batch_size or batch_size
+    def fit(self, train, batch_size=32, generator_batch_size=None, callbacks=None, workers=0):
         callbacks = callbacks or []
+        generator_batch_size = generator_batch_size or batch_size
+        sampler = train.get_sampler() if hasattr(train, "get_sampler") else None
+        loader = data.DataLoader(train, batch_size, sampler=sampler, shuffle=sampler is None,
+                                 drop_last=True, num_workers=workers, pin_memory=True)
 
+        self.g_kwargs["batch_size"] = generator_batch_size
         [c.set_trainer(self) for c in callbacks]
-        [c.set_training_config(dataset=train, epochs=epochs,
-                               batch_size=batch_size, g_batch_size=g_batch_size) for c in callbacks]
-        [c.on_train_begin() for c in callbacks]
+        [c.on_train_begin(loader=loader, generator_batch_size=generator_batch_size) for c in callbacks]
+        for e in count(start=0):
+            print("epoch ", e + 1)
 
-        for e in range(epochs):
-            print("epoch ", e + 1, "/", epochs)
             [c.on_epoch_begin(e) for c in callbacks]
-
             logs = self.train_one_epoch(loader, callbacks)
-
-            with torch.no_grad():
-                [c.on_epoch_end(e, logs) for c in callbacks]
+            [c.on_epoch_end(e, logs) for c in callbacks]
         [c.on_train_end() for c in callbacks]
 
-    def train_one_epoch(self, loader, callbacks):
-        logs = {}
-        n = len(loader)
-        metrics_names = ["d_loss", "g_loss"] + [m.__name__ for m in self.metrics]
-        running_metrics = np.zeros(len(metrics_names))
+    def train_one_epoch(self, train, callbacks):
+        n = len(train)
+        metric_names = ["d_loss", "g_loss"] + [m.__name__ for m in self.metrics]
+        running_metrics = np.zeros(len(metric_names) + 2)
 
         with utils.get_tqdm()(total=n) as pbar:
-            for i, x in enumerate(loader):
+            for i, xs in enumerate(train):
                 [c.on_batch_begin(i) for c in callbacks]
-                x = utils.to_device(x, device=self.x_device)
+                xs = utils.to_device(xs, device=self.device, return_array=True)
 
-                d_loss = self.train_discriminator(*x)
-                g_loss = self.train_generator(*x)
+                d_loss = self.train_discriminator(*xs)
+                g_loss = self.train_generator()
 
                 with torch.no_grad():
-                    metrics = self.get_metrics(d_loss, g_loss)
-                    running_metrics += metrics
+                    metrics = [float(m(self.generator_discriminator)) for m in self.metrics]
+                    metrics = [float(d_loss), float(g_loss)] + metrics
+                    running_metrics = running_metrics + metrics
 
-                    logs = dict(zip(metrics_names, running_metrics / (i + 1)))
-                    [c.on_batch_end(i, logs) for c in callbacks]
+                logs = dict(zip(metric_names, running_metrics / (i + 1)))
+                [c.on_batch_end(i, logs) for c in callbacks]
 
                 pbar.set_postfix(logs, refresh=False)
                 pbar.update()
@@ -78,53 +77,40 @@ class GANTrainer:
 
     def train_discriminator(self, *x):
         self.discriminator.train().zero_grad()
-        self.generator.train().zero_grad()
+        self.generator_optimizer.train().zero_grad()
 
-        x = [ag.Variable(a, requires_grad=True) for a in x] if self.r1 else x
-        real_score = self.discriminator(*x, **self.d_kwargs)
-        real_label = self.real_label * torch.ones(real_score.shape, dtype=real_score.dtype, device=real_score.device)
-        loss1 = self.loss(real_score, real_label)
+        if len(self.regularizers) > 0:
+            for a in x:
+                a.requires_grad = True
 
-        if self.r1 > 0:
-            grads = ag.grad(real_score.sum(), x, retain_graph=True, create_graph=True, only_inputs=True)
+        real_scores = self.discriminator(*x)
+        real_labels = self.real_label * torch.ones(real_scores.shape, device=real_scores.device)
+        real_loss = self.loss(real_scores, real_labels)
 
-            grad_loss = 0
-            for g in grads:
-                grad_loss = grad_loss + g.pow(2).sum(dim=[2, 3])
-            loss1 = loss1 + grad_loss.mean() * self.r1 / 2
+        if len(self.regularizers) > 0:
+            for r in self.regularizers:
+                real_loss = real_loss + r(x, real_scores)
 
-        loss1.backward()
+        real_loss.backward()
 
-        with torch.no_grad():
-            fakes = self.generator(**self.g_kwargs)
-            fakes = fakes if not torch.is_tensor(fakes) else [fakes]
-        fake_score = self.discriminator(*fakes, **self.d_kwargs)
-        fake_label = self.fake_label * torch.ones(fake_score.shape, dtype=fake_score.dtype, device=fake_score.device)
-        loss2 = self.loss(fake_score, fake_label)
-        loss2.backward()
+        fake_scores = self.generator_discriminator(generator_grad=False, **self.g_kwargs)
+        fake_labels = self.fake_label * torch.ones(fake_scores.shape, device=fake_scores.device)
+        fake_loss = self.loss(fake_scores, fake_labels)
+        fake_loss.backward()
 
-        self.d_optimizer.step()
-        loss = loss1 + loss2
+        self.discriminator_optimizer.step()
+        loss = fake_loss + real_loss
 
         return loss.detach()
 
-    def train_generator(self, *_):
-        self.discriminator.train().zero_grad()
-        self.generator.train().zero_grad()
+    def train_generator(self):
+        self.generator_discriminator.train().zero_grad()
 
-        fake = self.generator(**self.g_kwargs)
-        fake_score = self.discriminator(fake, **self.d_kwargs)
-        real_label = self.real_label * torch.ones(fake_score.shape, dtype=fake_score.dtype, device=fake_score.device)
-        loss = self.loss(fake_score, real_label)
+        fake_scores = self.generator_discriminator(generator_grad=True, **self.g_kwargs)
+        real_labels = self.real_label * torch.ones(fake_scores.shape, device=fake_scores.device)
+        loss = self.loss(fake_scores, real_labels)
         loss.backward()
 
-        self.g_optimizer.step()
+        self.generator_optimizer.step()
 
         return loss.detach()
-
-    def get_metrics(self, d_loss, g_loss):
-        self.generator.eval()
-        metrics = [float(m(self.generator)) for m in self.metrics]
-        metrics = [float(d_loss), float(g_loss), *metrics]
-
-        return np.array(metrics)
