1acd43ccc7cc85da91bc10edf869c8f8e8bcef42
jokingbear <nerokid1072@gmail.com>
2020-04-23 11:25:02 +0700

    remove init

--- plasma/training/__init__.py
+++
@@ -1,3 +0,0 @@
-from plasma.training.data import StandardDataset as Dataset, RandomSampler, SequentialSampler
-from plasma.training.trainer import StandardTrainer as Trainer
-from plasma.training.utils import on_notebook, eval_model
