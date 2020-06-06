3d3c4ec8d33c04629ccf36f2593a9895656ec9f3
Simon Huynh <nerokid1072@gmail.com>
2020-06-06 12:31:24 +0700

    add epoch end

--- test.py
+++ test.py
@@ -1,12 +1,4 @@
-import numpy as np
-import pandas as pd
-
-shuffled_idc = np.random.choice(15, size=15, replace=False)
-mapping_idc = sorted([(old, new) for new, old in enumerate(shuffled_idc)], key=lambda kv: kv[0])
-inversed_idc = [new for _, new in mapping_idc]
+import torch.hub as hub
 
 
-a = np.random.randn(15)
-b = a[shuffled_idc]
-c = b[inversed_idc]
-pd.DataFrame(np.stack([a, b, c], axis=-1), columns=["original", "shuffle", "inverse"])
+hub.load("jokingbear/MaskCheX:develop", "custom_vgg", tmp=5)
