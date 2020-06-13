dad09ec6973726b388c746bfa0c96cb0a79a1ca6
Simon Huynh <nerokid1072@gmail.com>
2020-06-13 10:40:55 +0700

    fix crop bug

--- plasma/training/data/augmentations/custom_crop.py
+++ plasma/training/data/augmentations/custom_crop.py
@@ -1,3 +1,4 @@
+import cv2
 import numpy as np
 
 from albumentations import DualTransform
@@ -5,10 +6,11 @@ from albumentations import DualTransform
 
 class MinEdgeCrop(DualTransform):
 
-    def __init__(self, size, always_apply=False, p=0.5):
+    def __init__(self, size, interpolation=cv2.INTER_LINEAR, always_apply=False, p=0.5):
         super().__init__(always_apply, p)
 
         self.size = size
+        self.interpolation = interpolation
 
     def apply_to_bbox(self, bbox, **params):
         raise NotImplementedError("no implementation for bbox")
@@ -16,27 +18,29 @@ class MinEdgeCrop(DualTransform):
     def apply(self, img, **params):
         h, w = img.shape[:2]
 
-        assert h % 2 == 0 and w % 2 == 0
+        assert h % 2 == 0 and w % 2 == 0, "height and width must be even"
 
         min_edge = min(h, w)
         position = params["position"]
         if h > min_edge:
             if position == "left":
-                return img[:min_edge]
+                img = img[:min_edge]
             elif position == "center":
                 d = (h - min_edge) // 2
-                return img[d:-d]
+                img = img[d:-d]
             else:
-                return img[-min_edge:]
+                img = img[-min_edge:]
 
         if w > min_edge:
             if position == "left":
-                return img[:, :min_edge]
+                img = img[:, :min_edge]
             elif position == "center":
                 d = (w - min_edge) // 2
-                return img[:, d:-d]
+                img = img[:, d:-d]
             else:
-                return img[-min_edge:]
+                img = img[:, -min_edge:]
+
+        return cv2.resize(img, (self.size, self.size), interpolation=self.interpolation)
 
     def get_params(self):
         return {
