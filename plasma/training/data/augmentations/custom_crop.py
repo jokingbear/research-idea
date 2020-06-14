60c70b419f53186d3d42836c3a899d660e07bfd3
jokingbear <nerokid1072@gmail.com>
2020-06-14 19:24:36 +0700

    refactor custom crop

--- plasma/training/data/augmentations/custom_crop.py
+++ plasma/training/data/augmentations/custom_crop.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 
 from albumentations import DualTransform
@@ -6,28 +5,33 @@ from albumentations import DualTransform
 
 class MinEdgeCrop(DualTransform):
 
-    def __init__(self, size, interpolation=cv2.INTER_LINEAR, always_apply=False, p=0.5):
+    def __init__(self, always_apply=False, p=0.5):
         super().__init__(always_apply, p)
 
-        self.size = size
-        self.interpolation = interpolation
+    def apply_to_keypoint(self, keypoint, **params):
+        raise NotImplementedError("no implementation for bbox")
+
+    def get_transform_init_args_names(self):
+        return super().get_transform_init_args_names()
 
     def apply_to_bbox(self, bbox, **params):
         raise NotImplementedError("no implementation for bbox")
 
-    def apply(self, img, **params):
+    def apply(self, img, position="center"):
         h, w = img.shape[:2]
 
         assert h % 2 == 0 and w % 2 == 0, "height and width must be even"
 
         min_edge = min(h, w)
-        position = params["position"]
         if h > min_edge:
             if position == "left":
                 img = img[:min_edge]
             elif position == "center":
                 d = (h - min_edge) // 2
                 img = img[d:-d]
+
+                if h % 2 != 0:
+                    img = img[1:]
             else:
                 img = img[-min_edge:]
 
@@ -37,11 +41,14 @@ class MinEdgeCrop(DualTransform):
             elif position == "center":
                 d = (w - min_edge) // 2
                 img = img[:, d:-d]
+
+                if w % 2 != 0:
+                    img = img[:, 1:]
             else:
                 img = img[:, -min_edge:]
 
         assert img.shape[0] == img.shape[1], f"height and width must be the same, currently {img.shape[:2]}"
-        return cv2.resize(img, (self.size, self.size), interpolation=self.interpolation)
+        return img
 
     def get_params(self):
         return {
