40c9d3274255645f99cb22b2916358ad4ac5d5f3
Simon Huynh <nerokid1072@gmail.com>
2020-06-18 16:52:30 +0700

    update graph.py

--- plasma/modules/graph.py
+++ plasma/modules/graph.py
@@ -50,10 +50,10 @@ def get_label_correlation(df, columns, return_count=True):
 def get_adjacency_matrix(smooth_corr, neighbor_ratio=0.2):
     identiy = np.identity(smooth_corr.shape[0])
     reweight = smooth_corr - identiy
-    reweight = reweight * neighbor_ratio / (1 - neighbor_ratio) / (reweight.values.sum(axis=0, keepdims=True) + 1e-6)
+    reweight = reweight * neighbor_ratio / (1 - neighbor_ratio) / (reweight.values.sum(axis=0, keepdims=True) + 1e-8)
     reweight = reweight + identiy
 
     D = reweight.values.sum(axis=1) ** (-0.5)
     D = np.diag(D)
     normalized = D @ reweight.values.transpose() @ D
-    return normalized
+    return pd.DataFrame(normalized, index=smooth_corr.index, columns=smooth_corr.columns)
