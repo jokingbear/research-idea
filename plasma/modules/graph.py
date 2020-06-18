f37ae1d9c365c8c53579e03ae84b3dab18f5264d
jokingbear <nerokid1072@gmail.com>
2020-06-18 19:20:32 +0700

    add documentation

--- plasma/modules/graph.py
+++ plasma/modules/graph.py
@@ -33,6 +33,13 @@ class GraphLinear(nn.Linear):
 
 
 def get_label_correlation(df, columns, return_count=True):
+    """
+    Calculate correlation of columns from data frame
+    :param df: pandas dataframe
+    :param columns: colunms to calculate correlation
+    :param return_count: return occurrence count
+    :return: correlation and counts
+    """
     counts = pd.DataFrame(columns=columns, index=columns)
 
     for c1 in columns:
@@ -48,6 +55,12 @@ def get_label_correlation(df, columns, return_count=True):
 
 
 def get_adjacency_matrix(smooth_corr, neighbor_ratio=0.2):
+    """
+    Get adjacency matrix from smoothed correlation
+    :param smooth_corr: smoothed correlation matrix as dataframe
+    :param neighbor_ratio: how strong neighbor nodes affect main nodes
+    :return: adjacency matrix as dataframe
+    """
     identiy = np.identity(smooth_corr.shape[0])
     reweight = smooth_corr - identiy
     reweight = reweight * neighbor_ratio / (1 - neighbor_ratio) / (reweight.values.sum(axis=0, keepdims=True) + 1e-8)
