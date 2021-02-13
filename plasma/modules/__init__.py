287e6aad0cfb0e4b91a65a7ba72831d2c4b02cf1
jokingbear <nerokid1072@gmail.com>
2021-02-13 19:52:51 +0700

    add template

--- plasma/modules/__init__.py
+++ plasma/modules/__init__.py
@@ -1,4 +1,4 @@
 from .attentions import SEAttention, CBAM, SAModule, DSAModule
 from .commons import *
 from .graph import GraphLinear, GraphSequential, GCN
-from ..modules import graph, tta
+from ..modules import graph, tta, templates
