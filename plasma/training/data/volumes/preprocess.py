6fccbfdb2bf41c6b2ecb32058629e5b5e779cf7b
jokingbear <nerokid1072@gmail.com>
2021-05-18 12:02:28 +0700

    add preprocess for volumes

--- plasma/training/data/volumes/preprocess.py
+++ plasma/training/data/volumes/preprocess.py
@@ -13,3 +13,22 @@ def resample(vol, dxyz, new_dxyz, order=1):
     new_vol = zoom(vol, size, order=order)
 
     return new_vol
+
+
+def crop_or_pad(vol, axis, size):
+    original_size = vol.shape[axis]
+
+    total_diff = original_size - size
+    if total_diff == 0:
+        return vol
+
+    diff = abs(total_diff) // 2
+    left = diff
+    right = (diff + 1) if total_diff % 2 != 0 else diff
+
+    if total_diff > 0:
+        slices = [slice(None, None) if a != axis else slice(left, -right) for a, _ in enumerate(vol.shape)]
+        return vol[tuple(slices)]
+    else:
+        pad = [(0, 0) if a != axis else (left, right) for a, _ in enumerate(vol.shape)]
+        return np.pad(vol, pad, constant_values=vol.min())
--- test.py
+++ test.py
@@ -1,8 +1,12 @@
-import torch
-import torch.nn as nn
+import numpy as np
+
+from plasma.training.data.volumes import preprocess
 
 
-a = torch.rand(5, 8, 3, 3, 3)
+a = np.array([0, 1, 2, 3, 4])
+b = np.stack([a, a + 5, a + 10, a + 15, a + 20], axis=1)
+c1 = preprocess.crop_or_pad(b, 1, 2)
+c2 = preprocess.crop_or_pad(c1, 0, 2)
 
-
-mean, std = torch.std_mean(a, dim=[1, 2, 3, 4], keepdim=True)
+c3 = preprocess.crop_or_pad(c2, 0, 5)
+c4 = preprocess.crop_or_pad(c3, 1, 5)
