53cd9f0cdcddfd2ab0d599d7b6acd6dd1ec5e1b1
jokingbear <nerokid1072@gmail.com>
2022-08-19 12:32:37 +0700

    remove equivariant convolution

--- plasma/modules/__init__.py
+++ plasma/modules/__init__.py
@@ -1,4 +1,3 @@
 from .attentions import SEAttention, SAModule
 from .commons import *
-from .graph import GraphLinear, GraphSequential, GCN
-from ..modules import graph, tta, s4
+from ..modules import graph
