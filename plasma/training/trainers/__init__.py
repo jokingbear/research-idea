b9d32e04b87a294e431ca863e6b5596e00d10370
Simon Huynh <nerokid1072@gmail.com>
2023-11-01 20:47:53 +0700

    remove training code

--- plasma/training/trainers/__init__.py
+++
@@ -1,8 +0,0 @@
-from .standard_trainer import StandardTrainer as Trainer
-from .deepspeed_trainer import DeepspeedTrainer
-
-
-__mapping__ = {
-    "standard": Trainer,
-    #'deepspeed': DeepspeedTrainer,
-}
