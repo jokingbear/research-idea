bb5f5bdc4ac4f97c02ef1c1570ae382f657e1f82
simon.huynh <nerokid1072@gmail.com>
2023-11-21 11:01:09 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='plasma',
+    version='1.0.0',
+    packages=['plasma', 'plasma.hub', 'plasma.modules', 'plasma.training', 'plasma.training.data',
+              'plasma.search_engines'],
+    url='',
+    license='',
+    author='jokingbear',
+    author_email='',
+    description=''
+)
