97e6f2417081172baad9ff20d4fad03ab861c531
Simon Huynh <nerokid1072@gmail.com>
2023-11-22 17:38:49 +0700

    add submodule walkthrough

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+import loralib
+
 import plasma.search_engines as engines
 import networkx as nx
 
@@ -7,3 +9,14 @@ matcher = engines.SequenceMatcher(db)
 
 query = 'tiểu đường là gì, và ảnh hưởng thế nào đến huyết áp'
 matcher.match_query(query,)
+
+
+import torchvision.models as pretrains
+import loralib as ll
+import torch.nn as nn
+
+
+a = nn.Linear(18, 18, bias=True)
+
+b = ll.Linear(18, 18, r=16)
+b.load_state_dict(a.state_dict(), strict=False)
