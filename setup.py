eda5453b1b35dbb7011cc8440614999d99ccae8e
simon.huynh <nerokid1072@gmail.com>
2023-12-14 14:06:28 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='plasma',
-    version='1.2.4',
-    packages=['plasma', 'plasma.hub', 'plasma.modules', 'plasma.training', 'plasma.training.data',
+    version='1.4.4',
+    packages=['plasma', 'plasma.meta', 'plasma.modules', 'plasma.training', 'plasma.training.data',
               'plasma.search_engines'],
     url='',
     license='',
