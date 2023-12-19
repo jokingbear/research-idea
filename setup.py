99f4fbc8e92e6040666cd1bba2ae42286a1ec5e0
joking.bear <nerokid1072@gmail.com>
2023-12-19 10:55:50 +0700

    update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 
 setup(
     name='plasma',
-    version='1.4.4',
     packages=['plasma', 'plasma.meta', 'plasma.logging', 'plasma.modules', 'plasma.training', 'plasma.training.data',
-              'plasma.search_engines'],
+              'plasma.search_engines', 'plasma.parallel_processing'],
     url='',
     license='',
     author='jokingbear',
