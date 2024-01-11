13c349dce4c2e5a582c3a030150779957d2a017c
simon.huynh <nerokid1072@gmail.com>
2024-01-11 13:51:06 +0700

    add streamline path versioning

--- plasma/huggingface/utils.py
+++ plasma/huggingface/utils.py
@@ -4,23 +4,28 @@ from huggingface_hub import hf_hub_download, snapshot_download
 from ..meta import import_module
 
 
-def download_module(repo_id, patterns=('*.py', '*.json', '*.yaml', '*.yml'), local_dir='dependencies', revision=None):
+def download_module(repo_id, patterns=('*.py', '*.json', '*.yaml', '*.yml'), local_dir='dependencies'):
     module_name = repo_id.split('/')[-1]
-
+    module_name_revision = module_name.split('@')
+    module_name = module_name[0]
+    revision = None if len(module_name_revision) == 1 else module_name_revision[1]
     path = f'{local_dir}/{module_name}'
     path = snapshot_download(repo_id, allow_patterns=patterns, local_dir=path, revision=revision)
 
     return import_module(path)
 
 
-def download_checkpoint(repo_id_filename, device='cpu', local_dir=None, revision=None):
-    path = download_file(repo_id_filename, local_dir, revision)
+def download_checkpoint(repo_id_filename, device='cpu', local_dir=None):
+    path = download_file(repo_id_filename, local_dir)
 
     return torch.load(path, map_location=device)
 
 
-def download_file(repo_id_filename, local_dir=None, revision=None):
+def download_file(repo_id_filename, local_dir=None):
     repo_id, filename = repo_id_filename.split(':')
+    filename_revision = filename.split('@')
+    filename = filename_revision[0]
+    revision = None if len(filename_revision) == 1 else filename_revision[1]
     path = hf_hub_download(repo_id, filename, local_dir=local_dir, revision=revision)
 
     return path
