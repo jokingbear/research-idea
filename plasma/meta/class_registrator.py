a9fac2506ee25d249224578d46d7e09e3b49f6e8
simon.huynh <nerokid1072@gmail.com>
2024-02-02 15:48:25 +0700

    revamp class registration + add mass import

--- plasma/meta/class_registrator.py
+++ plasma/meta/class_registrator.py
@@ -3,9 +3,19 @@ import warnings
 
 class ObjectFactory(dict):
 
-    def register(self, name, class_or_func):
-        if name in self:
-            warnings.warn(f'many entry points for: {name}, overriding the current one')
-        self[name] = class_or_func
+    def register(self, name):
+        return object_map(self, name)
 
-        return class_or_func
+
+class object_map:
+
+    def __init__(self, factory: ObjectFactory, name):
+        self._factory = factory
+        self.name = name
+
+        if self.name in self._factory:
+            warnings.warn(f'many entry points for: {self.name}, overriding the current one')
+
+    def __call__(self, func_or_class):
+        self._factory[self.name] = func_or_class
+        return func_or_class
--- plasma/meta/entry_factory.py
+++ plasma/meta/entry_factory.py
@@ -4,7 +4,7 @@ _factory = ObjectFactory()
 
 
 def entry_point(func_or_class):
-    return _factory.register(func_or_class.__module__, func_or_class)
+    return _factory.register(func_or_class.__module__)(func_or_class)
 
 
 def get_module_entry(module):
