56b10392862dc387561a5bd9e98f76c14f1d575b
simon.huynh <nerokid1072@gmail.com>
2024-03-12 14:34:50 +0700

    fix incorrect chain

--- plasma/parallel_processing/chained_block.py
+++ plasma/parallel_processing/chained_block.py
@@ -3,9 +3,9 @@ from .base_block import Block
 
 class ChainedBlock(Block):
 
-    def __init__(self, block1, create_block_func, *args, **kwargs):
+    def __init__(self, block1, create_block_func, func, output_queue, *args, **kwargs):
         assert isinstance(block1, Block), f'block1 must be an instance of {Block.__qualname__}'
-        block2 = create_block_func(*args, **kwargs)
+        block2 = create_block_func(func, block1.inputs, output_queue, *args, **kwargs)
 
         assert isinstance(block2, Block), f'block2 must return an instance of {Block.__qualname__}'
         super().__init__(block1.inputs, block2.outputs)
