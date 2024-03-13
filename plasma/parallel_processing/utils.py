be7b3978cd36e20197ef4e69266a506b8a478990
simon.huynh <nerokid1072@gmail.com>
2024-03-13 22:48:47 +0700

    standardize sync queue interface

--- plasma/parallel_processing/utils.py
+++ plasma/parallel_processing/utils.py
@@ -3,22 +3,32 @@ import time
 from queue import Queue
 
 
-def sync_queues_determine(total, input_queue: Queue, output_queue: Queue):
+def sync_queues_determine(q1: Queue, q2: Queue, total, return_items=False):
+    items = []
     for _ in range(total):
-        x = input_queue.get()
-        output_queue.put(x)
+        x = q1.get()
+        items.append(x)
+        q2.put(x)
+    
+    if return_items:
+        return items
 
 
-def sync_queues_undetermine(q1: Queue, q2: Queue, delay_time=0.5, loop_time=0.1):
+def sync_queues_undetermine(q1: Queue, q2: Queue, delay_time=0.5, loop_time=0.1, return_items=False):
     if delay_time is not None:
         time.sleep(delay_time)
     
     counter = 0
+    items = []
     while q1.qsize() > 0:
         x = q1.get()
         q2.put(x)
         if loop_time is not None:
             time.sleep(loop_time)
         counter += 1
+        items.append(x)
     
+    if return_items:
+        return counter, items
+
     return counter
--- setup.py
+++ setup.py
@@ -2,12 +2,10 @@ from setuptools import setup
 
 setup(
     name='plasma',
-    version='1.8.7',
+    version='1.8.8',
     packages=['plasma', 'plasma.meta', 'plasma.logging', 'plasma.modules', 'plasma.training', 'plasma.training.data',
               'plasma.functional', 'plasma.huggingface', 'plasma.search_engines', 'plasma.parallel_processing'],
-    url='',
-    license='',
+    url='https://github.com/jokingbear/research-idea',
+    license='MIT',
     author='jokingbear',
-    author_email='',
-    description=''
 )
