11253719e64edd67b59be665ae6d4104c7333244
simon.huynh <nerokid1072@gmail.com>
2024-04-13 16:32:23 +0700

    add logging module

--- plasma/functional/pipe.py
+++ plasma/functional/pipe.py
@@ -1,12 +1,14 @@
 import re
 
 from abc import abstractmethod
+from .utils import partials
 
 
 class Pipe:
 
     def __init__(self, **kwargs):
         self._marked_attributes = []
+        self._hooks = []
 
         for attr, val in kwargs.items():
             self._marked_attributes.append(attr)
@@ -15,6 +17,11 @@ class Pipe:
     @abstractmethod
     def run(self, *inputs, **kwargs):
         pass
+    
+    def add_logger(self, logging_func):
+        self._hooks.append(logging_func)
+
+        self.run = _HookRunner(self)
 
     def __call__(self, *args, **kwargs):
         return self.run(*args, **kwargs)
@@ -37,5 +44,25 @@ class Pipe:
 
         rep = ''.join(rep)
         rep = '\n' + rep
-        rep = re.sub('\([\t\n]{1,}\)', '()', rep)
+        rep = re.sub(r'\([\t\n]{1,}\)', '()', rep)
         return f'{type(self).__name__}({rep})'
+
+
+class _HookRunner:
+
+    def __init__(self, pipe: Pipe) -> None:
+        self._pipe = pipe
+        self._original_func = pipe.run
+    
+    def __call__(self, *args, **kwargs):
+        inputs = {
+            'args': args,
+            'kwargs': kwargs
+        }
+
+        outputs = self._original_func(*args, **kwargs)
+
+        for logger in self._pipe._hooks:
+            logger(inputs, outputs)
+
+        return outputs
