5ad58ec438c70b750ad8e03d01f14492c8bd5f44
simon.huynh <nerokid1072@gmail.com>
2024-04-13 20:23:58 +0700

    fix sequential pipe bug

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ packages = [re.sub(r'\\|\/', '.', str(p.parent)) for p in Path('plasma').rglob('
 
 setup(
     name='plasma',
-    version='1.9.9',
+    version='1.9.10',
     packages=['plasma', *packages],
     url='https://github.com/jokingbear/research-idea',
     license='MIT',
