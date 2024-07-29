d529c94ce1ad351e9a4a9c4ca629a5aff4fcd07d
simon.huynh <nerokid1072@gmail.com>
2024-07-29 12:51:20 +0700

    add shared init for object factory

--- plasma/meta/class_registrator.py
+++ plasma/meta/class_registrator.py
@@ -1,10 +1,25 @@
 import warnings
+import pandas as pd
 
 
 class ObjectFactory(dict):
 
     def register(self, *names, verbose=True):
         return object_map(self, names, verbose)
+    
+    def shared_init(self, *args, **kwargs):
+        obj_dict = {}
+        results = {}
+        for k, initiator in self.items():
+            if initiator not in k:
+                obj_dict[initiator] = initiator(*args, **kwargs)
+            
+            results[k] = obj_dict[initiator]
+
+        return results
+    
+    def normal_init(self, *args, **kwargs):
+        return {k: initiator(*args, **kwargs) for k, initiator in self.items()}
 
 
 class object_map:
