a3c12b66210bc31dcf187768d30fbfb92a6e30f5
simon.huynh <nerokid1072@gmail.com>
2024-09-12 11:05:27 +0700

    update logic of path inquirer for easier problem data registration

--- plasma/functional/path_dict.py
+++ plasma/functional/path_dict.py
@@ -7,45 +7,44 @@ class ObjectInquirer:
         self.original_object = obj
 
         self._registered_obj_getter = {
-            dict: _identity,
-            list: _identity,
-            pd.DataFrame: _dataframe,
-            pd.Series: _series
+            dict: _item_getter,
+            list: _item_getter,
+            pd.DataFrame: _dataframe_iloc,
+            pd.Series: _series_loc
         }
     
     def register_getter(self, obj_type:type, func):
         self._registered_obj_getter[obj_type] = func
 
     def __getitem__(self, path):
-        obj_type = type(self.original_object)
-        getter = self._registered_obj_getter.get(obj_type, None)
-        if getter is None:
-            raise NotImplementedError(f'there is no key getter for object of type {obj_type}, please call register function')
-
-        data = getter(self.original_object)
-
         if isinstance(path, list):
-            obj = data[path[0]]
-
-            if len(path) == 1:
-                return obj
-            else:
-                return type(self)(obj)[path[1:]]
+            current_key = path[0]
+            next_path = path[1:]
         else:
-            return data[path]
+            current_key = path
+            next_path = []
+        
+        obj_type = type(self.original_object)
+        getter = self._registered_obj_getter.get(obj_type, getattr)
+      
+        value = getter(self.original_object, current_key)
+
+        if len(next_path) > 0:
+            return ObjectInquirer(value)[next_path]
+
+        return value
 
     def __repr__(self) -> str:
         return repr(self.original_object)
 
 
-
-def _identity(obj):
-    return obj
-
-
-def _dataframe(df:pd.DataFrame):
-    return df.iloc
+def _item_getter(obj, key):
+    return obj[key]
 
 
-def _series(s:pd.Series):
-    return s.loc
+def _dataframe_iloc(df:pd.DataFrame, idx):
+    return df.iloc[idx]
+
+
+def _series_loc(s:pd.Series, key):
+    return s.loc[key]
