f7dcae3a9ffe6b8066177b7296f2b0d8edb8fd15
simon.huynh <nerokid1072@gmail.com>
2024-09-30 20:57:46 +0700

    hotfix sequential pipe

--- plasma/functional/sequential_pipe.py
+++ plasma/functional/sequential_pipe.py
@@ -3,6 +3,12 @@ from .pipe import AutoPipe
 
 class SequentialPipe(AutoPipe):
 
+    def __init__(self, **pipes):
+        super().__init__()
+
+        for key, pipe in pipes.items():
+            self.__setattr__(key, pipe)
+
     def __setattr__(self, key:str, value):
         if key[0] != '_':
             assert isinstance(value, AutoPipe), 'attribute must be instance of AutoPipe'
