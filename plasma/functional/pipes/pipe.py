84400e4198b51c7ce726d5de652c0f904c32e358
simon.huynh <nerokid1072@gmail.com>
2024-11-14 23:17:03 +0700

    remove hook log

--- plasma/functional/pipes/pipe.py
+++ plasma/functional/pipes/pipe.py
@@ -7,16 +7,11 @@ class AutoPipe[T]:
 
     def __init__(self):
         self._marked_attributes = []
-        self._hooks = []
 
     @abstractmethod
     def run(self, *inputs, **kwargs) -> T:
         pass
     
-    def add_logger(self, logging_func):
-        self._hooks.append(logging_func)
-        self.run = _HookRunner(self)
-
     def __setattr__(self, key:str, value):
         if key[0] != '_' and key not in self._marked_attributes:
             self._marked_attributes.append(key)
@@ -46,29 +41,3 @@ class AutoPipe[T]:
         rep = '\n' + rep
         rep = re.sub(r'\([\t\n]{1,}\)', '()', rep)
         return f'{type(self).__name__}({rep})'
-
-
-class _HookRunner:
-
-    def __init__(self, pipe:AutoPipe) -> None:
-        self._pipe = pipe
-        self._original_func = pipe.run
-    
-    def __call__(self, *args, **kwargs):
-        inputs = {
-            'args': args,
-            'kwargs': kwargs
-        }
-
-        try:
-            outputs = self._original_func(*args, **kwargs)
-        except Exception as e:
-            outputs = e
-
-        for logger in self._pipe._hooks:
-            logger(inputs, outputs)
-
-        if isinstance(outputs, Exception):
-            raise outputs
-
-        return outputs
