9def0c72b8b3994f2441c00fb6c85e0cf71f98e9
simon.huynh <nerokid1072@gmail.com>
2024-11-15 00:00:35 +0700

    add structure to logging module

--- plasma/logging/timer.py
+++ plasma/logging/timer.py
@@ -1,30 +1,32 @@
 import time
 
-from ..functional import partials, proxy_func
 from functools import wraps
+from collections import namedtuple
 
 
 class Timer:
+    TimeIO = namedtuple('TimeIO', ['name', 'duration', 'args', 'kwargs'])
 
-    def __init__(self, log_func=print) -> None:
+    def __init__(self, log_func=print, log_inputs=False) -> None:
         self.log_func = log_func
-        self.start = None
-        self.end = None
+        self.log_inputs = log_inputs
+        self._start = None
+        self._end = None
 
     def __enter__(self):
-        self.start = time.time()
+        self._start = time.time()
         return self
 
     def __exit__(self, *_):
-        self.end = time.time()
+        self._end = time.time()
         if self.log_func is None:
-            print(self.duration)
+            print(f'{self.duration:.2f}s')
 
     @property
     def duration(self):
-        if self.start is None or self.end is None:
+        if self._start is None or self._end is None:
             raise ValueError('please call enter and exit method appropriately')
-        return self.end - self.start
+        return self._end - self._start
 
     def __call__(self, func):
         name = func.__qualname__
@@ -33,8 +35,8 @@ class Timer:
         def run_timer(*args, **kwargs):
             with self:
                 results = func(*args, **kwargs)
-            duration = self.duration
-            self.log_func({name: f'{duration:.2f}s'})
+            timeio = self.TimeIO(name, self.duration, args if self.log_inputs else [], kwargs if self.log_inputs else {})
+            self.log_func(timeio)
             return results
 
         return run_timer
--- plasma/torch/modules/__init__.py
+++ plasma/torch/modules/__init__.py
@@ -1,3 +1,2 @@
-from .algebraic_module import AlgebraicModule
 from .attentions import SEAttention, SAModule
 from .commons import *
