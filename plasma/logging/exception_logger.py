b19845b2bb512e3662644d813ac6663eb5a3d834
simon.huynh <nerokid1072@gmail.com>
2024-11-25 20:28:39 +0700

    add exception logger

---
+++ plasma/logging/exception_logger.py
@@ -0,0 +1,30 @@
+from functools import wraps
+from collections import namedtuple
+
+
+class ExceptionLogger:
+    ExceptionIO = namedtuple('ExIO', ['name', 'args', 'kwargs', 'exception'])
+
+    def __init__(self, name=None, log_func=print, raise_on_exception=True, exception_return=None) -> None:
+        self.name = name
+        self.log_func = log_func
+        self.raise_on_exception = raise_on_exception
+        self.exception_value = exception_return
+    
+    def __call__(self, function):
+        name = self.name or function.__qualname__
+        
+        @wraps(function)
+        def run(*args, **kwargs):
+            try:
+                results = function(*args, **kwargs)
+                return results
+            except Exception as e:
+                exio = self.ExceptionIO(name, args, kwargs, e)
+                self.log_func(exio)
+                
+                if self.raise_on_exception:
+                    raise e
+                return self.exception_value
+
+        return run
