f25f7d3d5ae5b728e56708a74fd140cd659529d0
simon.huynh <nerokid1072@gmail.com>
2024-12-11 12:31:11 +0700

    quick fix to timeio

--- plasma/logging/timer.py
+++ plasma/logging/timer.py
@@ -2,11 +2,10 @@ import time
 import datetime
 
 from functools import wraps
-from collections import namedtuple
+from dataclasses import dataclass
 
 
 class Timer:
-    TimeIO = namedtuple('TimeIO', ['name', 'start', 'duration', 'args', 'kwargs'])
 
     def __init__(self, log_func=print, log_inputs=False) -> None:
         self.log_func = log_func
@@ -29,7 +28,7 @@ class Timer:
         if self._start is None or self._end is None:
             raise ValueError('please call enter and exit method appropriately')
     
-        return self.end - self.start
+        return datetime.timedelta(seconds=self._end - self._start)
 
     @property
     def start(self):
@@ -46,8 +45,22 @@ class Timer:
         def run_timer(*args, **kwargs):
             with self:
                 results = func(*args, **kwargs)
-            timeio = self.TimeIO(name, self.start, self.duration, args if self.log_inputs else [], kwargs if self.log_inputs else {})
+            timeio = TimeIO(name, self, args if self.log_inputs else [], kwargs if self.log_inputs else {})
             self.log_func(timeio)
             return results
 
         return run_timer
+
+    def __repr__(self):
+        return f'(start={self.start}, end={self.end}, duration={self.duration})'
+
+
+@dataclass
+class TimeIO:
+    name:str
+    timer:Timer
+    args:list
+    kwargs:dict
+
+
+Timer.TimeIO = TimeIO
