0019da8a75105635f84f5d6bbdc0e3f0a80cf7ff
simon.huynh <nerokid1072@gmail.com>
2024-12-26 11:51:52 +0700

    add chain dataset

--- plasma/torch/data/__init__.py
+++ plasma/torch/data/__init__.py
@@ -1,3 +1,4 @@
 from .adhoc_data import AdhocData
 from .base_class import BaseDataset
 from .array_data import DynamicDataset
+from .chain import ChainDataset
---
+++ plasma/torch/data/chain.py
@@ -0,0 +1,20 @@
+from .base_class import BaseDataset
+
+
+class ChainDataset(BaseDataset):
+
+    def __init__(self, *datasets:BaseDataset):
+        super().__init__()
+
+        self.datasets = datasets
+    
+    def get_len(self):
+        return sum([len(d) for d in self.datasets], 0)
+    
+    def get_item(self, idx):
+        offset = 0
+        for ds in self.datasets:
+            if offset <= idx < offset + len(ds):
+                return ds[idx - offset]
+
+        raise IndexError(f'index {idx} is out of bound')
