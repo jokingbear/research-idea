873a34debf5cec2e01d11757cb7d1654ef858252
simon.huynh <nerokid1072@gmail.com>
2025-01-06 20:00:30 +0700

    make the api more sensible

--- plasma/torch/data/array.py
+++ plasma/torch/data/array.py
@@ -1,6 +1,7 @@
 import pandas as pd
 
 from .base import BaseDataset
+from abc import abstractmethod
 
 
 class DynamicDataset(BaseDataset):
@@ -13,13 +14,17 @@ class DynamicDataset(BaseDataset):
     def get_len(self):
         return len(self._data)
     
-    def __getitem__(self, idx):
-        item = self.pick_element(idx)
-        return self.get_item(item)
+    def get_item(self, idx):
+        item = self.pick_item(idx)
+        return self.process_item(item)
     
-    def pick_element(self, idx):
+    def pick_item(self, idx):
         data = self._data
         if isinstance(data, (pd.DataFrame, pd.Series)):
             return data.iloc[idx]
         else:
             return data[idx]
+
+    @abstractmethod
+    def process_item(self, item):
+        pass
