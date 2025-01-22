014701b5f9d5504db2565c01e11285bb8598cf45
simon.huynh <nerokid1072@gmail.com>
2025-01-22 10:11:32 +0700

    refactor code for more understandable

--- plasma/parallel_processing/queues/utils.py
+++ plasma/parallel_processing/queues/utils.py
@@ -3,13 +3,13 @@ from .signals import Signal
 
 
 def internal_run(queue:Queue, persistent, callback):
-    is_continued = True
-    while is_continued:
+    is_not_cancelled = True
+    while is_not_cancelled:
         data = queue.get()
         exception = None
 
-        is_continued = data is not Signal.CANCEL
-        if is_continued:
+        is_not_cancelled = data is not Signal.CANCEL
+        if is_not_cancelled:
             try:
                 callback(data)
             except Exception as e:
