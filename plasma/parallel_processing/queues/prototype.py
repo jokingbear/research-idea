e7513347ee3dbf915f554cb9472ef15abef2791b
simon.huynh <nerokid1072@gmail.com>
2025-01-27 20:06:48 +0700

    fix queue prototype

--- plasma/parallel_processing/queues/prototype.py
+++ plasma/parallel_processing/queues/prototype.py
@@ -4,6 +4,6 @@ from .base import Queue
 class QueuePrototype[T](Queue[T]):
 
     def __init__(self, block):
-        super().__init__()
+        super().__init__(block)
 
         print('this class is deprecated, please use Queue')
