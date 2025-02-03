119697dceca7bbf23aec2b2f156831f1792e2a08
simon.huynh <nerokid1072@gmail.com>
2025-02-04 00:51:49 +0700

    add init to features class

--- plasma/torch/interfaces/visions/feature_extractor.py
+++ plasma/torch/interfaces/visions/feature_extractor.py
@@ -5,17 +5,20 @@ from abc import abstractmethod
 
 
 class Features:
-    globals: list[torch.Tensor]
-    intermediates: list[torch.Tensor]
+
+    def __init__(self, globals: list[torch.Tensor], intermedidates: list[torch.Tensor]):
+        self.globals = globals
+        self.intermediates = intermedidates
 
     def __repr__(self):
         tab = ' ' * 2
 
         return (
-            'Features(\n'
+            'Features\n'
             f'{tab}globals={[f.shape for f in self.globals]},\n'
-            f'{tab}intermediates={[fi.shape for fi in self.intermediates]}'
+            f'{tab}intermediates={[fi.shape for fi in self.intermediates]}\n'
         )
+
 
 class FeatureExtractor(Vision):
 
