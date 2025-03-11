f8d363b7fafe77ff037e018ca484feef83229513
simon.huynh <nerokid1072@gmail.com>
2025-03-11 09:10:32 +0700

    add propagator

--- plasma/functional/decorators/utils.py
+++ plasma/functional/decorators/utils.py
@@ -12,3 +12,17 @@ def automap(func):
 
     return alt_func
 
+
+class propagate:
+
+    def __init__(self, value=None):
+        self.value = value
+    
+    def __call__(self, func):
+        def alt_func(inputs):
+            if inputs is self.value:
+                return self.value
+            else:
+                return func(inputs)
+
+        return alt_func
