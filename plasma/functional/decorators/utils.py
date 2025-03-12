b334a90712113b4ffa19e77cad5d0e93090d77f9
simon.huynh <nerokid1072@gmail.com>
2025-03-12 08:36:30 +0700

    fix automap for instance method

--- plasma/functional/decorators/utils.py
+++ plasma/functional/decorators/utils.py
@@ -1,17 +1,26 @@
+import inspect
+
 from functools import wraps
 
 
 def automap(func):
+    signature = inspect.getfullargspec(func)
+    is_instance_method = signature.args[0] == 'self'
 
-    def alt_func(inputs):
+    def alt_func(*inputs):
+        instance = []
+        if is_instance_method:
+            instance = [inputs[0]]
+            inputs = inputs[1]
+
         if isinstance(inputs, (tuple, list)):
-            return func(*inputs)
+            return func(*instance, *inputs)
         elif isinstance(inputs, dict):
-            return func(**inputs)
+            return func(*instance, **inputs)
         elif inputs is None:
-            return func()
+            return func(*instance)
         else:
-            return func(inputs)
+            return func(*instance, inputs)
 
     return alt_func
 
