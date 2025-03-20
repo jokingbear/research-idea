2bc5505cef33e4825dae91bbfcc59fc9961d7b3b
simon.huynh <nerokid1072@gmail.com>
2025-03-20 12:13:58 +0700

    add ignore signal acknowledge

--- plasma/parallel_processing/queues/process.py
+++ plasma/parallel_processing/queues/process.py
@@ -3,6 +3,7 @@ import multiprocessing as mp
 from .signals import Signal
 from .utils import internal_run
 from .base import Queue
+from ...functional.decorators import propagate
 
 
 class ProcessQueue(Queue[list[mp.Process]]):
@@ -20,6 +21,7 @@ class ProcessQueue(Queue[list[mp.Process]]):
         [p.start() for p in processes]
         return processes
 
+    @propagate(Signal.CANCEL)
     def put(self, x):
         self._queue.put(x)
     
