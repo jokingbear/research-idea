86e9cb9d3a86c8d4d52f5dadc82ab60bd355e000
simon.huynh <nerokid1072@gmail.com>
2025-04-16 12:58:58 +0700

    expose handler

--- plasma/parallel_processing/communicators/__init__.py
+++ plasma/parallel_processing/communicators/__init__.py
@@ -2,3 +2,4 @@ from .sequential import Sequential, Flow
 from .aggregator import Aggregator
 from . import distributors
 from .tree2 import StableTree as TreeFlow
+from .handler import FlowExceptionHandler
