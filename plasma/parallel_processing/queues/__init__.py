17040aa42162122424a2ea79b3d6f197e76af363
simon.huynh <nerokid1072@gmail.com>
2025-05-08 09:00:27 +0700

    remove old prototype

--- plasma/parallel_processing/queues/__init__.py
+++ plasma/parallel_processing/queues/__init__.py
@@ -3,6 +3,5 @@ from .process import ProcessQueue
 from .signals import Signal
 from .base import Queue
 from .transfer import TransferQueue
-from .prototype import QueuePrototype
 from queue import Full, Empty
 from .handler import ExceptionHandler
