d31636fa084b2ce337ec4db1404070198ae88394
simon.huynh <nerokid1072@gmail.com>
2025-06-11 13:41:43 +0700

    fix graph indexer

--- plasma/search_engines/graph_indexer.py
+++ plasma/search_engines/graph_indexer.py
@@ -1,6 +1,7 @@
 import plasma.functional as F
 import pandas as pd
 import networkx as nx
+import numpy as np
 
 from .regex_splitter import RegexTokenizer
 from .token_matcher import TokenMatcher
@@ -52,13 +53,15 @@ class GraphIndexer(F.SimplePipe[str, pd.DataFrame]):
             path_frames = pd.concat(path_frames, axis=0, ignore_index=True)
             path_frames = path_frames.merge(sentence_frame, on='sentence')
             path_frames['query_start_idx'] = path_frames['start_idx'] + path_frames['query_start_idx']
-            path_frames['query_end_idx'] = path_frames['end_idx'] + path_frames['query_start_idx']
+            path_frames['query_end_idx'] = path_frames['start_idx'] + path_frames['query_end_idx']
         else:
             path_frames = pd.DataFrame(columns=columns)
             
         path_frames = path_frames[columns]\
             .groupby(['query_start_idx', 'query_end_idx'])\
-                .apply(_post_process, self.path_walker.top_k, include_groups=False)
+                .apply(_post_process, self.path_walker.top_k, include_groups=False)\
+                    .droplevel(None)
+        path_frames = _find_largest_intervals(path_frames)
         return path_frames
     
     def _run_sentence(self, sentence:str):
@@ -74,3 +77,13 @@ class GraphIndexer(F.SimplePipe[str, pd.DataFrame]):
 
 def _post_process(df:pd.DataFrame, topk):
     return df.sort_values(['substring_matching_score', 'coverage_score'], ascending=False).iloc[:topk]
+
+
+def _find_largest_intervals(df:pd.DataFrame):
+    intervals = {(s, e) for s, e, *_ in df.index}
+    intervals = np.array(list(intervals))
+    
+    cond_counts = ((intervals[:, 0] <= intervals[:, 0, np.newaxis]) & (intervals[:, 1, np.newaxis] <= intervals[:, 1])).sum(axis=1)
+    cond_counts = cond_counts == 1
+    filtered = intervals[cond_counts]
+    return df.loc[[(int(s), int(e)) for s, e in filtered]].sort_index()
